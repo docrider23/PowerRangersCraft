a6f33343fc13f0000801e0c98a52f168365ee626
docrider23 <drwhofan23@live.com>
2026-01-08 16:36:25 -0500

    Added Empty Engine Cells

--- src/main/java/com/docrider/powerrangerscraft/items/RPMItems.java
+++ src/main/java/com/docrider/powerrangerscraft/items/RPMItems.java
@@ -20,19 +20,22 @@ public class RPMItems {
     public static final DeferredItem<Item> RPM_LOGO = ITEMS.register("rpm_logo",
             () -> new BaseItem(new Item.Properties()).AddToTabList(RangerTabs.RPM));
 
+    public static final DeferredItem<Item> EMPTY_ENGINE_CELL = ITEMS.register("empty_engine_cell",
+            () -> new BaseItem(new Item.Properties()).AddToTabList(RangerTabs.RPM));
+
     public static final DeferredItem<Item> EAGLE_RACER_ENGINE_CELL = ITEMS.register("eagle_racer_engine_cell",
             () -> new RangerFormChangeItem(new Item.Properties(),0,"","rpm_red","rpm_cell_shift_belt")
                     .AddToTabList(RangerTabs.RPM));
 
     public static final DeferredItem<Item> RPM_HELMET = ITEMS.register("rpm_head",
-            () -> new RangerArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new Item.Properties()).AddToTabList(RangerTabs.RPM).ChangeRepairItem(RPM_LOGO.get()));
+            () -> new RangerArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new Item.Properties()).AddToTabList(RangerTabs.RPM).ChangeRepairItem(EMPTY_ENGINE_CELL.get()));
     public static final DeferredItem<Item> RPM_CHESTPLATE = ITEMS.register("rpm_torso",
-            () -> new RangerArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new Item.Properties()).AddToTabList(RangerTabs.RPM).ChangeRepairItem(RPM_LOGO.get()));
+            () -> new RangerArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new Item.Properties()).AddToTabList(RangerTabs.RPM).ChangeRepairItem(EMPTY_ENGINE_CELL.get()));
     public static final DeferredItem<Item> RPM_LEGGINGS = ITEMS.register("rpm_legs",
-            () -> new RangerArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.LEGGINGS, new Item.Properties()).AddToTabList(RangerTabs.RPM).ChangeRepairItem(RPM_LOGO.get()));
+            () -> new RangerArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.LEGGINGS, new Item.Properties()).AddToTabList(RangerTabs.RPM).ChangeRepairItem(EMPTY_ENGINE_CELL.get()));
 
     public static final DeferredItem<Item> RED_CELL_SHIFT_MORPHER = ITEMS.register("red_cell_shift_morpher",
-            () -> new CellShiftMorpherItem(ArmorMaterials.DIAMOND,"rpm_red", EAGLE_RACER_ENGINE_CELL,RPM_HELMET,RPM_CHESTPLATE,RPM_LEGGINGS,new Item.Properties()).Add_Extra_Base_Form_Items(OtherItems.BLANK_FORM).AddToTabList(RangerTabs.RPM).ChangeRepairItem(RPM_LOGO.get()));
+            () -> new CellShiftMorpherItem(ArmorMaterials.DIAMOND,"rpm_red", EAGLE_RACER_ENGINE_CELL,RPM_HELMET,RPM_CHESTPLATE,RPM_LEGGINGS,new Item.Properties()).Add_Extra_Base_Form_Items(OtherItems.BLANK_FORM).AddToTabList(RangerTabs.RPM).ChangeRepairItem(EMPTY_ENGINE_CELL.get()));
 
 
     public static void register(IEventBus eventBus) {
